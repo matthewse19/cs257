f32a1eb37dbf468beb8633131790d90f85a2e1c6
matthewse19 <mcse@maine.rr.com>
2021-01-13 15:43:45 -0600

    Updated some methods

--- books/books.py
+++ books/books.py
@@ -1,19 +1,44 @@
 import argparse
 import csv
+import sys
 
 def get_parsed_arguments():
     parser = argparse.ArgumentParser(description='Filter and display information about books.csv')
     parser.add_argument('-t', "--titles", default="", nargs='?', help='Phrase in title to filter by')
     parser.add_argument('-a', "--authors", default="", nargs="?", help='Phrase in author\'s name to filter by')
-    parser.add_argument('-y', "--years", nargs=2, type=int, help='Range of publication years to filter by')
-    #parser.add_argument('--test', '-t', action="store_true", help='test text')
+    parser.add_argument('-y', "--years", metavar="YEAR", nargs=2, type=int, help='Range of publication years to filter by')
+    parser.add_argument('-o', "--order", default="", nargs="?", help='[title, author, year] Which category to sort by and how to display')
+    parser.add_argument('-d', "--descending", action="store_true", help='Sets to descending')
+
     parsed_arguments = parser.parse_args()
     return parsed_arguments
+
+def print_titles(book_list, descending):
+    book_list.sort(reverse = descending, key=lambda x: x[0])
+    for book in book_list:
+        print(book[0])
+
+def print_years(book_list, descending):
+    for book in book_list:
+        print(book[0], ",", book[1])
+
+def print_authors(book_list, descending):
+    authors_books = {}
+    for book in book_list:
+        if book[2] in authors_books:
+            authors_books[book[2]].append(book[0])
+        else:
+            authors_books[book[2]] = [book[0]]
+
+    for author in authors_books:
+        print(author)
+        for book in authors_books[author]:
+            print("\t", book)
+        print("\n")
 
 def main():
     arguments = get_parsed_arguments()
     print(arguments)
-
     books = []
     with open('books.csv', newline='') as f:
         reader = csv.reader(f)
@@ -30,23 +55,23 @@ def main():
                     continue
             books.append(row)
 
-    print_authors(books)
-
-def print_titles(book_list):
-    for book in book_list:
-        print(book[0])
-
-def print_years(book_list):
-    for book in book_list:
-        print(book[0], ",", book[1])
-
-def print_authors(book_list):
-    authors_books = {}
-    for book in book_list:
-        if book[2] in authors_books:
-            authors_books[book[2]].append(book[0])
+    if arguments.order == "title":
+        print_titles(books, arguments.descending)
+    elif arguments.order =="year":
+        print_years(books)
+    elif arguments.order =="author":
+        print_authors(books)
+    elif arguments.order == "":
+        if arguments.titles != "":
+            print_titles(books, arguments.descending)
+        elif arguments.authors != "":
+            print_authors(books)
+        elif arguments.years != None:
+            print_years(books)
         else:
-            authors_books[book[2]] = [book[0]]
-    print(authors_books)
+            print("No search argument was inputed, type books.py --help to see commands", file=sys.stderr)
+    else:
+        print("Inputed incorrect order parameter, must be 'title', 'author', or 'year'", file=sys.stderr)
+
 if __name__ == '__main__':
     main()
