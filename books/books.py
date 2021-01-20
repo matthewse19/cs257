41dd3e60c033114255df48b2f2a4ab57872917b2
matthewse19 <mcse@maine.rr.com>
2021-01-20 15:44:08 -0600

    did revisions to books.py and changed usage.txt accordingly

--- books/books.py
+++ books/books.py
@@ -1,52 +1,100 @@
 #Riaz Kelly, Matthew Smith-Erb
+#Revised by Riaz Kelly and Matthew Smith-Erb
 import argparse
 import csv
 import sys
 
-#create a parser object to get arguments
 def get_parsed_arguments():
+    '''create and return a parser object to get arguments'''
     parser = argparse.ArgumentParser(description='Filter and display information about books.csv')
     parser.add_argument('-t', "--titles", default="", nargs='?',
         help='Phrase in title to filter by')
     parser.add_argument('-a', "--authors", default="", nargs="?",
         help='Phrase in author\'s name to filter by')
-    parser.add_argument('-y', "--years", metavar="YEAR", nargs=2, type=int,
-        help='Range of publication years to filter by')
-    parser.add_argument('-o', "--order", default="", nargs="?",
-        help='[title, author, year] Which category to sort by and how to display')
+    parser.add_argument('-s', "--start_year", default = 0, nargs="?", type=int,
+        help='Lowest year to filter by')
+    parser.add_argument('-e', "--end_year", default = 2021, nargs="?", type=int,
+        help='Highest year to filter by')
+    parser.add_argument('-o', "--order", default="title", nargs="?",
+        help='[title, author, year] Which category to sort by and how to display, defaults to title')
     parser.add_argument('-d', "--descending", action="store_true",
         help='Order results in descending order instead of ascending')
 
     parsed_arguments = parser.parse_args()
     return parsed_arguments
 
-#print only the titles of books
 def print_titles(book_list, descending):
-    book_list.sort(reverse = descending, key=lambda x: x[0])
+    '''print only the titles of books alphabetically'''
+    title_index = 0
+    book_list.sort(reverse = descending, key=lambda x: x[title_index])
     for book in book_list:
-        print(book[0])
+        title = book[title_index]
+        print(title)
 
-#print the titles of books follow by their publication year
-def print_years(book_list, descending):
-    book_list.sort(reverse = descending, key=lambda x: x[1])
+def print_titles_years(book_list, descending):
+    '''print the titles of books followed by their year chronologically'''
+    title_index = 0
+    year_index = 1
+    book_list.sort(reverse = descending, key=lambda x: x[year_index])
     for book in book_list:
-        print(book[0], book[1])
+        title = book[title_index]
+        year = book[year_index]
+        print(title, year)
 
-#print the name of authors followed by the books they wrote matching the filter
 def print_authors(book_list, descending):
+    '''print the name of authors alphabetically followed by the books they wrote matching the filters'''
+    title_index = 0
+    author_index = 2
     #dict where keys are author's names and values are a list of strings of books they wrote
     authors_books = {}
+    #adds to authors_books according to which books fit the filter
     for book in book_list:
-        if book[2] in authors_books:
-            authors_books[book[2]].append(book[0])
+        title = book[title_index]
+        author = book[author_index]
+        if author in authors_books:
+            authors_books[author].append(title)
         else:
-            authors_books[book[2]] = [book[0]]
+            authors_books[author] = [title]
 
+    #sorts the authors_books based on authors' names
     for author in sorted(authors_books.keys(), reverse = descending):
         print(author)
         for book in authors_books[author]:
             print("\t", book)
         print("\n")
+
+def filter_books(reader, title_filter, author_filter, start_year, end_year):
+    '''returns a list of books from the reader object that matches the filters'''
+    books = []
+
+    for row in reader:
+        book_title = row[0].lower()
+        book_author = row[2].lower()
+        book_year = int(row[1])
+
+        if not(title_filter in book_title):
+            continue
+        if not(author_filter in book_author):
+            continue
+        if book_year > end_year or book_year < start_year:
+            continue
+
+        books.append(row)
+
+    return books
+
+def call_correct_print(order_type, descending, books):
+    '''calls one of three print functions based on order_type, defaults to print_titles()'''
+    if len(books) == 0:
+        print("No books matched your filters")
+    elif order_type == "title":
+        print_titles(books, descending)
+    elif order_type =="year":
+        print_titles_years(books, descending)
+    elif order_type =="author":
+        print_authors(books, descending)
+    else:
+        print("Inputed incorrect order parameter, must be 'title', 'author', or 'year'", file=sys.stderr)
 
 def main():
     arguments = get_parsed_arguments()
@@ -55,39 +103,16 @@ def main():
 
     with open('books.csv', newline='') as f:
         reader = csv.reader(f)
+        title_filter = arguments.titles.lower()
+        author_filter = arguments.authors.lower()
+        start_year = arguments.start_year
+        end_year = arguments.end_year
 
-        #adds entries into books if they fit the filter
-        for row in reader:
-            if not(arguments.titles.lower() in row[0].lower()):
-                continue
-            if not(arguments.authors.lower() in row[2].lower()):
-                continue
-            if arguments.years != None:
-                min_year = min(arguments.years[0], arguments.years[1])
-                max_year = max(arguments.years[0], arguments.years[1])
-                book_year = int(row[1])
-                if book_year > max_year or book_year < min_year:
-                    continue
-            books.append(row)
+        books = filter_books(reader, title_filter, author_filter, start_year, end_year)
 
-    #call the correct print method according to --order flag
-    if arguments.order == "title":
-        print_titles(books, arguments.descending)
-    elif arguments.order =="year":
-        print_years(books, arguments.descending)
-    elif arguments.order =="author":
-        print_authors(books, arguments.descending)
-    elif arguments.order == "":
-        if arguments.titles != "":
-            print_titles(books, arguments.descending)
-        elif arguments.authors != "":
-            print_authors(books, arguments.descending)
-        elif arguments.years != None:
-            print_years(books, arguments.descending)
-        else:
-            print("No search argument was inputed, type books.py --help to see commands", file=sys.stderr)
-    else:
-        print("Inputed incorrect order parameter, must be 'title', 'author', or 'year'", file=sys.stderr)
+    order_type = arguments.order
+    descending = arguments.descending
+    call_correct_print(order_type, descending, books)
 
 if __name__ == '__main__':
     main()
